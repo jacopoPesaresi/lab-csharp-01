92ad03696275c7414b53d1eddaaa3410fe4a97e3
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2022-12-06 18:55:57 +0100

    Last changes in exercice 2.

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -21,30 +21,59 @@ namespace ComplexAlgebra
     {
         public double Real { get; }
         public double Imaginary { get; }
+        public double Modulus 
+        { 
+            get => Math.Sqrt(Math.Pow(Real,2) + Math.Pow(Imaginary,2));
+        }
+        public double Phase 
+        { 
+            get => Math.Atan2(Imaginary,Real);
+        }
 
         public Complex (double real, double imm){
             Real = real;
             Imaginary = imm;
         }
 
-        public double Modulus() => Math.Sqrt(Math.Pow(Real,2)+Math.Pow(Imaginary,2));
-
-        public double Phase() => Math.Atan2(Imaginary,Real);
-
         public Complex Complement() => new Complex(Real, Imaginary*-1);
 
         public Complex Plus(Complex a) => new Complex (a.Real+Real, a.Imaginary+Imaginary);
 
-        public Complex Minus(Complex a) => new Complex (a.Real-Real, a.Imaginary-Imaginary);
+        public Complex Minus(Complex a) => new Complex (Real-a.Real, Imaginary-a.Imaginary);
         //valuta se usare la Complement, almeno che quella non mi inverte solo la parte Imaginaryaginaria
 
         public override string ToString() 
-            => 
-            //((Real > 0) ? "+" : (Real == 0) ? "" : "-") 
-            $"{Real}" 
-            //+ ((Imaginary > 0) ? "+" : (Imaginary == 0) ? "" : "-") 
-            + $"{Imaginary}";
-    
+        {
+            if (Real.Equals(0) && Imaginary.Equals(0))
+            {
+                return "0";
+            } 
+            else if (Real.Equals(0))
+            {
+                return (Math.Abs(Imaginary).Equals(1) ? "i" : $"{Imaginary}i");
+            }
+            else if (Imaginary.Equals(0))
+            {
+                return $"{Real}";
+            }
+            else
+            {
+                return  $"{Real} " 
+            + ((Imaginary > 0) ? "+ " : "- ") 
+            + ((Imaginary > 0) ? $"{Imaginary}" : (Imaginary == 0) ? "" : $"{-1*Imaginary}")
+            + ((Imaginary != 0) ? "i" : "");
+            }
+        }
+            //=> 
+            //$"{Real}"+$"{Imaginary}"+"i";
+            /*
+            //((Real > 0) ? "+" : "") 
+            $"{Real} " 
+            + ((Imaginary > 0) ? "+ " : (Imaginary == 0) ? " " : "- ") 
+            + ((Imaginary > 0) ? $"{Imaginary}" : (Imaginary == 0) ? "" : $"{-1*Imaginary}")
+            + ((Imaginary != 0) ? "i" : "");
+            //+ (Real != 0 ? "" : $"{Imaginary}");
+            */
         // TODO generate Equals(object obj)
         public override bool Equals(Object obj) {
 
