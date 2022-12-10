450fc034e8f798dde7e205a3bc91f050408783b0
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2022-12-10 23:39:33 +0100

    Exercise 2 done (few difference with 'Equals' and 'ToString')

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -19,41 +19,37 @@ namespace ComplexAlgebra
     /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
+        public Complex (double real, double imm)
+        {
+            Real = real;
+            Imaginary = imm;
+        }
+
         public double Real { get; }
+        
         public double Imaginary { get; }
+        
         public double Modulus 
         { 
             get => Math.Sqrt(Math.Pow(Real,2) + Math.Pow(Imaginary,2));
         }
+        
         public double Phase 
         { 
             get => Math.Atan2(Imaginary,Real);
         }
 
-        public Complex (double real, double imm){
-            Real = real;
-            Imaginary = imm;
-        }
-
-        public Complex Complement() => new Complex(Real, Imaginary*-1);
-
-        public Complex Plus(Complex a) => new Complex (a.Real+Real, a.Imaginary+Imaginary);
-
-        public Complex Minus(Complex a) => new Complex (Real-a.Real, Imaginary-a.Imaginary);
-        //valuta se usare la Complement, almeno che quella non mi inverte solo la parte Imaginaryaginaria
-
         public override string ToString() 
         {
-            if (Real.Equals(0) && Imaginary.Equals(0))
+            if (Real.Equals(0.0) && Imaginary.Equals(0.0))
             {
                 return "0";
             } 
-            else if (Real.Equals(0))
+            else if (Real.Equals(0.0))
             {
-                //return (Math.Abs(Imaginary).Equals(1) ? "i" : $"{Imaginary}i");
-                return (Imaginary.Equals(1) ? "i" : $"{Imaginary}i");
+                return (Imaginary.Equals(1.0) ? "i" : Imaginary.Equals(-1.0) ? "-i" : $"{Imaginary}i");
             }
-            else if (Imaginary.Equals(0))
+            else if (Imaginary.Equals(0.0))
             {
                 return $"{Real}";
             }
@@ -61,49 +57,35 @@ namespace ComplexAlgebra
             {
                 return  $"{Real} " 
             + ((Imaginary > 0) ? "+ " : "- ") 
-            + ((Imaginary > 0) ? $"{Imaginary}i" : $"{-1*Imaginary}i");
+            + ((Imaginary > 0) ? 
+                (Imaginary.Equals(1.0) ? "i" : $"{Imaginary}i") : 
+                (Imaginary.Equals(-1.0) ? "i" : $"{-1*Imaginary}i"));
             }
         }
-            //=> 
-            //$"{Real}"+$"{Imaginary}"+"i";
-            /*
-            //((Real > 0) ? "+" : "") 
-            $"{Real} " 
-            + ((Imaginary > 0) ? "+ " : (Imaginary == 0) ? " " : "- ") 
-            + ((Imaginary > 0) ? $"{Imaginary}" : (Imaginary == 0) ? "" : $"{-1*Imaginary}")
-            + ((Imaginary != 0) ? "i" : "");
-            //+ (Real != 0 ? "" : $"{Imaginary}");
-            */
-        // TODO generate Equals(object obj)
-        public override bool Equals(Object obj) {
 
+        public override bool Equals(Object obj) {
             if (obj == null || GetType() != obj.GetType())
             {
                 return false;
             }
-                        
             Complex comp = obj as Complex;
             if (comp != null)
             {
-                return comp.Real.CompareTo(Real)==0 && comp.Imaginary.CompareTo(Imaginary)==0;
+                return comp.Real.CompareTo(Real) == 0 && comp.Imaginary.CompareTo(Imaginary) == 0;
             }
             return false;
+        }
 
-        }
-        //    obj.Real.Equals(Name) && obj.Seed.Equals(Seed) && obj.Ordinal == Ordinal;
-        /*
+        public override int GetHashCode()
         {
-            if (obj istanceof Complex)
-            {
-                obj = (Complex)obj;
-                return obj.Real
-            }
-            else return false;
-        }
-        */
-        public override int GetHashCode(){
             return HashCode.Combine(Real, Imaginary);
         }
+        
+        public Complex Complement() => new Complex(Real, Imaginary*-1);
+
+        public Complex Plus(Complex a) => new Complex (a.Real+Real, a.Imaginary+Imaginary);
+
+        public Complex Minus(Complex a) => new Complex (Real-a.Real, Imaginary-a.Imaginary);
 
     }
 }
